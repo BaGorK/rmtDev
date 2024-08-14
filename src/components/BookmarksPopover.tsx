8271a793b5a7098f46afadde1cc3bb654b69ff2b
Edmealem kassahun <edmealemkassahun@gmail.com>
2024-08-14 10:39:55 +0300

    feat: create portal for popover

--- src/components/BookmarksPopover.tsx
+++ src/components/BookmarksPopover.tsx
@@ -1,14 +1,17 @@
+import { createPortal } from 'react-dom';
 import { forwardRef } from 'react';
+
 import { useBookmarkContext } from '../contexts/BookmarkProvider';
 import JobList from './JobList';
 
 const BookmarksPopover = forwardRef<HTMLDivElement>((_, ref) => {
   const { bookmarkedJobItems, isLoading } = useBookmarkContext();
 
-  return (
+  return createPortal(
     <div ref={ref} className='bookmarks-popover'>
       <JobList isLoading={isLoading} jobItems={bookmarkedJobItems} />
-    </div>
+    </div>,
+    document.querySelector('#modal')!
   );
 });
 
