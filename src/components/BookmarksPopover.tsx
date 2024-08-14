96a95afe9e864ea2817485566b0db566c4170d05
Edmealem kassahun <edmealemkassahun@gmail.com>
2024-08-14 09:31:41 +0300

    feat: close bookmarks popover on click outside

--- src/components/BookmarksPopover.tsx
+++ src/components/BookmarksPopover.tsx
@@ -1,18 +1,8 @@
 import { useBookmarkContext } from '../contexts/BookmarkProvider';
 import JobList from './JobList';
-import Spinner from './Spinner';
 
 export default function BookmarksPopover() {
   const { bookmarkedJobItems, isLoading } = useBookmarkContext();
-
-  if (isLoading) {
-    return (
-      <div className='bookmarks-popover'>
-        <Spinner />
-      </div>
-    );
-  }
-
 
   return (
     <div className='bookmarks-popover'>
